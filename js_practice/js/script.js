7cd189b8934e8a21c457b55104213583b6737d78
Sergi <sergiovsiannikov@gmail.com>
2021-08-28 09:31:13 +0200

    HW-functions

--- js_practice/js/script.js
+++ js_practice/js/script.js
@@ -1,10 +1,17 @@
 "use strict";
 
-// 1 task
-const numberOfFilms = +prompt("How many movies have you seen?", "");
-console.log(numberOfFilms);
+let numberOfFilms;
 
-// 2 task
+function start() {
+    numberOfFilms = +prompt("How many movies have you seen?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many movies have you seen?", "");
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -13,15 +20,53 @@ const personalMovieDB = {
     private: false
 };
 
-console.log(personalMovieDB);
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const answer1 = prompt("One of the latest movies you've seen?", ""),
+            answer2 = prompt("How would you rate it?", "");
 
-// 3 task
-const answer1 = prompt("One of the latest movies you've seen?", "");
-const answer2 = prompt("How would you rate it?", "");
+        if ((answer1 && answer2) !== null && (answer1 && answer2) !== '' && answer1.length < 50) {
+            personalMovieDB.movies[answer1] = answer2;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-personalMovieDB.movies[answer1] = answer2;
+rememberMyFilms();
 
-console.log(personalMovieDB.movies);
+function checkStatusOfTheCinematographerAmateur () {
+    if (personalMovieDB.count < 10) {
+        console.log('Not a lot of movies watched');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('You are a classic viewer');
+    } else if (personalMovieDB.count >= 30) {
+        console.log("You're a movie lover");
+    } else {
+        console.log('Error');
+    }
+}
+
+checkStatusOfTheCinematographerAmateur();
+
+function showMyDB() {
+    if (personalMovieDB.private === false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres.push(prompt(`Your favourite genre at number - ${i}`));
+    }
+}
+
+writeYourGenres();
+
 
 
 
